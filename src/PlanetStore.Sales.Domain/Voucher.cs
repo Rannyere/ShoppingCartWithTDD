4b4ebe0b468e1cf0be2cba2384f31182e0491667
Rannyere Almeida <rannyalmeida27@hotmail.com>
2021-08-17 15:34:16 +0200

    Create tests validate and Entity Voucher

---
+++ src/PlanetStore.Sales.Domain/Voucher.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using FluentValidation;
+using FluentValidation.Results;
+
+namespace PlanetStore.Sales.Domain
+{
+    public class Voucher
+    {
+        public string Code { get; private set; }
+        public decimal? DiscountPercentage { get; private set; }
+        public decimal? DiscountValue { get; private set; }
+        public DiscountTypeVoucher DiscountType { get; private set; }
+        public int Quantity { get; private set; }
+        public DateTime DateValidity { get; private set; }
+        public bool Activ { get; private set; }
+        public bool Usage { get; private set; }
+
+        public Voucher(string code, decimal? discountPercentage, decimal? discountValue, DiscountTypeVoucher discountType, int quantity, DateTime dateValidity, bool activ, bool usage)
+        {
+            Code = code;
+            DiscountPercentage = discountPercentage;
+            DiscountValue = discountValue;
+            DiscountType = discountType;
+            Quantity = quantity;
+            DateValidity = dateValidity;
+            Activ = activ;
+            Usage = usage;
+        }
+
+        public ValidationResult IsValidForUse()
+        {
+            return new VoucherValidation().Validate(this);
+        }
+    }
+
+    public class VoucherValidation : AbstractValidator<Voucher>
+    {
+        public static string CodeErroMsg => "Voucher without valid code.";
+        public static string DateValidityErroMsg => "This voucher has expired.";
+        public static string ActivErroMsg => "This voucher is no longer valid.";
+        public static string UsageErroMsg => "This voucher has already been used.";
+        public static string QuantityErroMsg => "This voucher is no longer available.";
+        public static string DiscountValueErroMsg => "The discount amount must be greater than 0";
+        public static string DiscountPercentageErroMsg => "The discount percentage value must be greater than 0";
+
+        public VoucherValidation()
+        {
+            RuleFor(c => c.Code)
+                .NotEmpty()
+                .WithMessage(CodeErroMsg);
+
+            RuleFor(c => c.DateValidity)
+                .Must(DataVencimentoSuperiorAtual)
+                .WithMessage(DateValidityErroMsg);
+
+            RuleFor(c => c.Activ)
+                .Equal(true)
+                .WithMessage(ActivErroMsg);
+
+            RuleFor(c => c.Usage)
+                .Equal(false)
+                .WithMessage(UsageErroMsg);
+
+            RuleFor(c => c.Quantity)
+                .GreaterThan(0)
+                .WithMessage(QuantityErroMsg);
+
+            When(f => f.DiscountType == DiscountTypeVoucher.Value, () =>
+            {
+                RuleFor(f => f.DiscountValue)
+                    .NotNull()
+                    .WithMessage(DiscountValueErroMsg)
+                    .GreaterThan(0)
+                    .WithMessage(DiscountValueErroMsg);
+            });
+
+            When(f => f.DiscountType == DiscountTypeVoucher.Percentage, () =>
+            {
+                RuleFor(f => f.DiscountPercentage)
+                    .NotNull()
+                    .WithMessage(DiscountPercentageErroMsg)
+                    .GreaterThan(0)
+                    .WithMessage(DiscountPercentageErroMsg);
+            });
+        }
+
+        protected static bool DataVencimentoSuperiorAtual(DateTime dataValidade)
+        {
+            return dataValidade >= DateTime.Now;
+        }
+    }
+}
+
+    public enum DiscountTypeVoucher
+    {
+        Percentage = 0,
+        Value = 1
+    }
+
---
+++ tests/PlanetStore.Sales.Domain.Tests/VoucherTests.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+
+namespace PlanetStore.Sales.Domain.Tests
+{
+    public class VoucherTests
+    {
+        [Fact(DisplayName = "Validate Voucher Type Value Valid")]
+        [Trait("Category", " Voucher")]
+        public void Voucher_ValidateVoucherTypeValueValid_MustBeValid()
+        {
+            // Arrange
+            var voucher = new Voucher("PROMO-15-EURO", null, 15, DiscountTypeVoucher.Value, 1, DateTime.Now.AddDays(15), true, false);
+
+            // Act
+            var result = voucher.IsValidForUse();
+
+            // Assert
+            Assert.True(result.IsValid);
+        }
+
+        [Fact(DisplayName = "Validate Voucher Type Value Invalid")]
+        [Trait("Category", " Voucher")]
+        public void Voucher_ValidateVoucherTypeValueInvalid_MustBeInvalid()
+        {
+            // Arrange
+            var voucher = new Voucher("", null, null, DiscountTypeVoucher.Value, 0, DateTime.Now.AddDays(-1), false, true);
+
+            // Act
+            var result = voucher.IsValidForUse();
+
+            // Assert
+            Assert.False(result.IsValid);
+            Assert.Equal(6, result.Errors.Count);
+            Assert.Contains(VoucherValidation.ActivErroMsg, result.Errors.Select(c => c.ErrorMessage));
+            Assert.Contains(VoucherValidation.CodeErroMsg, result.Errors.Select(c => c.ErrorMessage));
+            Assert.Contains(VoucherValidation.DateValidityErroMsg, result.Errors.Select(c => c.ErrorMessage));
+            Assert.Contains(VoucherValidation.QuantityErroMsg, result.Errors.Select(c => c.ErrorMessage));
+            Assert.Contains(VoucherValidation.UsageErroMsg, result.Errors.Select(c => c.ErrorMessage));
+            Assert.Contains(VoucherValidation.DiscountValueErroMsg, result.Errors.Select(c => c.ErrorMessage));
+        }
+
+        [Fact(DisplayName = "Validate Voucher Type Percentage Valid")]
+        [Trait("Category", " Voucher")]
+        public void Voucher_ValidateVoucherTypePercentageValid_MustBeValid()
+        {
+            // Arrange
+            var voucher = new Voucher("PROMO-15-EURO", 10, null, DiscountTypeVoucher.Percentage, 1, DateTime.Now.AddDays(15), true, false);
+
+            // Act
+            var result = voucher.IsValidForUse();
+
+            // Assert
+            Assert.True(result.IsValid);
+        }
+
+        [Fact(DisplayName = "Validate Voucher Type Percentage Invalid")]
+        [Trait("Category", " Voucher")]
+        public void Voucher_ValidateVoucherTypePercentageInvalid_MustBeInvalid()
+        {
+            // Arrange
+            var voucher = new Voucher("", null, null, DiscountTypeVoucher.Percentage, 0, DateTime.Now.AddDays(-1), false, true);
+
+            // Act
+            var result = voucher.IsValidForUse();
+
+            // Assert
+            Assert.False(result.IsValid);
+            Assert.Equal(6, result.Errors.Count);
+            Assert.Contains(VoucherValidation.ActivErroMsg, result.Errors.Select(c => c.ErrorMessage));
+            Assert.Contains(VoucherValidation.CodeErroMsg, result.Errors.Select(c => c.ErrorMessage));
+            Assert.Contains(VoucherValidation.DateValidityErroMsg, result.Errors.Select(c => c.ErrorMessage));
+            Assert.Contains(VoucherValidation.QuantityErroMsg, result.Errors.Select(c => c.ErrorMessage));
+            Assert.Contains(VoucherValidation.UsageErroMsg, result.Errors.Select(c => c.ErrorMessage));
+            Assert.Contains(VoucherValidation.DiscountPercentageErroMsg, result.Errors.Select(c => c.ErrorMessage));
+        }
+    }
+}
