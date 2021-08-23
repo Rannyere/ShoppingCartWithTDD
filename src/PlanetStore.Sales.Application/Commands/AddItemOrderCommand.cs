3910d3b2bbae009e44acc27e231680fc3a4fdf4c
Rannyere Almeida <rannyalmeida27@hotmail.com>
2021-08-23 10:29:17 +0200

    Create command AddItemOrderCommand and your first test

---
+++ src/PlanetStore.Sales.Application/Commands/AddItemOrderCommand.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using FluentValidation;
+using FluentValidation.Results;
+using PlanetStore.Sales.Application.Commands;
+using PlanetStore.Sales.Domain;
+
+namespace PlanetStore.Sales.Application.Commands
+{
+    public class AddItemOrderCommand
+    {
+        public Guid CustomerId { get; set; }
+        public Guid ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int Quantity { get; set; }
+        public decimal UnitValue { get; set; }
+
+        public AddItemOrderCommand(Guid customerId, Guid productId, string productName, int quantity, decimal unitValue)
+        {
+            CustomerId = customerId;
+            ProductId = productId;
+            ProductName = productName;
+            Quantity = quantity;
+            UnitValue = unitValue;
+        }
+
+        public bool IsValid()
+        {
+            return new AddItemOrderCommandValidation().Validate(this).IsValid;
+        }
+    }
+}
+
+public class AddItemOrderCommandValidation : AbstractValidator<AddItemOrderCommand>
+{
+    public static string CustomerIdErrorMsg => "Invalid customer id";
+    public static string ProductIdErrorMsg => "Invalid product id";
+    public static string ProductNameErrorMsg => "The product name was not informed";
+    public static string QuantityMaxErrorMsg => $"The maximum quantity of an item is {Order.MAX_UNITS_ITEM}";
+    public static string QuantityMinErrorMsg => "The minimum quantity of an item is 1";
+    public static string UnitValueErrorMsg => "Item value must be greater than 0";
+
+    public AddItemOrderCommandValidation()
+    {
+        RuleFor(c => c.CustomerId)
+            .NotEqual(Guid.Empty)
+            .WithMessage(CustomerIdErrorMsg);
+
+        RuleFor(c => c.ProductId)
+            .NotEqual(Guid.Empty)
+            .WithMessage(ProductIdErrorMsg);
+
+        RuleFor(c => c.ProductName)
+            .NotEmpty()
+            .WithMessage(ProductNameErrorMsg);
+
+        RuleFor(c => c.Quantity)
+            .GreaterThan(0)
+            .WithMessage(QuantityMinErrorMsg)
+            .LessThanOrEqualTo(Order.MAX_UNITS_ITEM)
+            .WithMessage(QuantityMaxErrorMsg);
+
+        RuleFor(c => c.UnitValue)
+            .GreaterThan(0)
+            .WithMessage(UnitValueErrorMsg);
+    }
+}
+
---
+++ tests/PlanetStore.Sales.Application.Tests/Orders/AddItemOrderCommandTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using PlanetStore.Sales.Application.Commands;
+using Xunit;
+
+namespace PlanetStore.Sales.Application.Tests.Orders
+{
+    public class AddItemOrderCommandTests
+    {
+        private readonly Guid _customerId;
+        private readonly Guid _productId;
+
+        public AddItemOrderCommandTests()
+        {
+            _customerId = Guid.NewGuid();
+            _productId = Guid.NewGuid();
+        }
+
+        [Fact(DisplayName = "Add Item Command Valid ")]
+        [Trait("Category", "Order Commands Tests")]
+        public void AddItemOrderCommand_CommandIsValid_MustBeValid()
+        {
+            // Arrange
+            var orderCommand = new AddItemOrderCommand(_customerId, _productId, "Product Test", 2, 100);
+
+            // Act
+            var result = orderCommand.IsValid();
+
+            //Assert
+            Assert.True(result);
+        }
+    }
+}
